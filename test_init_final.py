cdad8deb0dcf2a936c8377afbc4691f3d9071a5b
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-12 18:45:39 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1390,7 +1390,7 @@ while True:
 				
 			################ 보탐봇 음성채널 소환 ################ 
 
-			if message.content.startswith(command[4]) or message.content.startswith(command[12]):
+			if message.content.startswith(command[12]) or message.content.startswith(command[4]):
 				if message.author.voice == None:
 					await client.get_channel(channel).send('음성안내는 음성방에 입장하셔야 안내합니다.', tts=False)
 				else:
