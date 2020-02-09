3d2a881429a7a3d187a7e84a96d6a425fb31c4e9
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-09 23:02:45 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -997,7 +997,7 @@ while True:
 			
 			################ 텍스트 정보확인 ################ 
 		@client.event
-		async def on_message(msg):			
+		async def on_message(msg) and def on_message(message):			
 
 			if msg.content == command[2]:
 				ch_information = ''
@@ -1053,13 +1053,12 @@ while True:
 			
 			
 			
-
+			hello = message.content
 			##################################
 
 			for i in range(bossNum):
 				################ 보스 컷처리 ################ 
 				if message.content.startswith(bossData[i][0] +'컷'):
-					hello = message.content
 					if hello.find('  ') != -1 :
 						bossData[i][6] = hello[hello.find('  ')+2:]
 						hello = hello[:hello.find('  ')]
